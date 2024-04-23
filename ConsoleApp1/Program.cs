22da9e8a336648ea6479b2a5cc04975bec5573d9
dealtime101 <dealtime101@hotmail.com>
2024-04-22 23:31:39 -0400

    Exercice #1
    First Exercice
    
    Exercice : Quel est votre age?

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Veuillez entrez votre nom.");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Veuillez maintenant entrez votre âge.");
+            string age = Console.ReadLine();
+
+            Console.WriteLine("Bonjour, vous vous appelez " + name + " et votre âge est : " + age);
+        }
+    }
+}
