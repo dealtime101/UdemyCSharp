0fbe8084b4c81c7d007a648794d392ae541de163
dealtime101 <dealtime101@hotmail.com>
2024-04-30 22:58:57 -0400

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,29 +6,70 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            string nom = "";
 
-            Console.Write("Quel est ton nom ? ");
-            string nom = Console.ReadLine();
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            while (nom == "" || (int.TryParse(nom, out int _)))
+            {
+                Console.Write("Quel est ton nom ? ");
+                nom = Console.ReadLine();
+                nom.Trim();
+
+                try
+                {
+
+                    if (nom == "")
+                    {
+                        Console.WriteLine("Erreur: Le nom ne doit pas être vide");
+                    }
+                    else if (int.TryParse(nom, out int _))
+                    {
+                        Console.WriteLine("Erreur: Le nom ne doit pas être un nombre");
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+
+
+
 
             int age_num = 0;
 
-            while (age_num == 0)
+            while (age_num <= 0)
             {
                 Console.Write("Quel est ton age ? ");
                 string age_str = Console.ReadLine();
 
                 try
                 {
+                    int? age_null = age_num;
                     age_num = int.Parse(age_str);
+
+
+                    if (age_null == null)
+                    {
+                        Console.WriteLine("Erreur: l'âge ne doit pas être vide");
+                    }
+
+                    if (age_num < 0)
+                    {
+                        Console.WriteLine("Erreur: L'âge ne doit pas être négatif");
+                    }
+                    if (age_num == 0)
+                    {
+                        Console.WriteLine("Erreur: L'âge ne doit pas être zéro");
+                    }
                 }
                 catch
                 {
-                    Console.WriteLine("Erreur, vous devez rentrer un age valide.");
+                    Console.WriteLine("Erreur: vous devez rentrer un age valide.");
                 }
-            }
 
-            // ici age_num est forcément différent de 0
+
+            }
 
             Console.WriteLine("Bonjour, vous vous appelez " + nom + ", vous avez " + age_num + " ans");
 
